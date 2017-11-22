1239f98d6ac30438c32078f3531901cf739c3b62
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-11-22 15:52:19 +0800

    Tree OK ready for KNN algorithm

--- MLHW2_KDTREE_KNN/kdtree_ver2.py
+++ MLHW2_KDTREE_KNN/kdtree_ver2.py
@@ -57,33 +57,59 @@ def tree_traverse_check(current_kd_node,cnt):
 
 def validate(root,training_set):
     validation_set = []
-
     for i in range(0,36):
         validation_set.append(training_set[i])
     #what to output
-    output_file = open('result.txt','w') #append mode
+    output_file = open('result.txt','w') #Write mode
+    #integer declaration
     predicted_correct = 0
-    #knn 1
+    #string class declaration
+    original_class = None
+    final_predicted_class = None
+    #hashmap declaration for voting system
+    knn_result_hash = {'cp':0,'im':0,'pp':0,'imU':0,'om':0,'omL':0,'imL':0,'imS':0}
+    classname_set = ['cp','im','pp','imU','om','omL','imL','imS']
+    #KNN main core
     first_three_output = [[] for i in range(3)]
     for knn_query in [1,5,10,100]:
+        first_three_output = [[] for i in range(3)]
+        predicted_correct += 1
         for query_index in range(0,36):
-            query_point = validation_set[i] #take the point for querying
+            query_point = validation_set[query_index] #take the point for querying
+            original_class = query_point[11]
+            for search_hash in range(len(classname_set)): #clear the hash map for the query from each point for voting
+                knn_result_hash[classname_set[search_hash]] = 0
 
+            tree_traverse_check(root,0) #clear all traversed mark to false first,which symbolized non traversed
             for individual_knn_query in range(0,knn_query):
-                original_class = query_point[11]
-                tree_traverse_check(root,0) #clear all to false first
-                NN,predicted_class = KNN_core(root,query_point)
 
-                if(original_class == predicted_class):
-                    predicted_correct+=1
+                NN,predicted_class = KNN_core(root,query_point)
+                knn_result_hash[predicted_class] += 1
                 if(query_index >=0 and query_index<3): #outputresult
                     first_three_output[query_index].append(NN)
 
-            output_file.write('KNN accuracy: '+str(float(predicted_correct/36.0)))
-            output_file.write(first_three_output[0])
-            output_file.write(first_three_output[1])
-            output_file.write(first_three_output[2])
+            max_voted_class = 0
+            for search_hash in range(len(classname_set)): #voting for the best result
+                if knn_result_hash[classname_set[search_hash]] > max_voted_class:
+                    max_voted_class = knn_result_hash[classname_set[search_hash]]
+                    final_predicted_class = knn_result_hash[classname_set[search_hash]]
 
+            if(original_class == final_predicted_class):
+                predicted_correct+=1
+
+        print("A KNN Is end ",query_index,"Acc is")
+        output_file.write('KNN accuracy: '+str(float(predicted_correct/36.0))+'\n')
+        for output_index in range(len(first_three_output[0])):
+            output_file.write(first_three_output[0][output_index]+' ')
+        output_file.write('\n')
+        for output_index in range(len(first_three_output[1])):
+            output_file.write(first_three_output[1][output_index]+' ')
+        output_file.write('\n')
+        for output_index in range(len(first_three_output[2])):
+            output_file.write(first_three_output[2][output_index]+' ')
+        output_file.write('\n')
+
+        output_file.write('\n')
 
     output_file.close()
 
