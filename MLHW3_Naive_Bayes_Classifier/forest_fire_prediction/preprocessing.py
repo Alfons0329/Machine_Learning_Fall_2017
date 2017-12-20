ce7b80823e291d600c0abae46c1d2d93a74e21ac
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-12-21 00:52:01 +0800

    Now working on machine learning HW3

--- MLHW3_Naive_Bayes_Classifier/forest_fire_prediction/preprocessing.py
+++ MLHW3_Naive_Bayes_Classifier/forest_fire_prediction/preprocessing.py
@@ -8,12 +8,14 @@ import csv
 import sys
 import math
 
-def draw_PDF():
+def preprocesing():
     all_data_name = sys.argv[1]
     with open(all_data_name,'r') as opened_file: #use r for reading a file
         parsed_data = csv.reader(opened_file)
         all_data_list = list(parsed_data)
-
+    return 
+def draw_PDF():
+    for i in len
     all_data_list = np.array(all_data_list)#temporary convert to numpy array for plotting the pdf
     feature_1 = all_data_list[:,4]
     feature_2 = all_data_list[:,5]
@@ -34,4 +36,5 @@ def draw_PDF():
     input()
 
 if __name__ == '__main__':
-    draw_PDF()
+    preprocesing():
+    draw_PDF():
