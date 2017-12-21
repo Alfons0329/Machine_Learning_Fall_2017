d33816fe8e0a3a315482c4faf860c9699e24a06d
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-12-21 19:24:57 +0800

    Ready for the PDF of forest fire

--- MLHW3_Naive_Bayes_Classifier/forest_fire_prediction/preprocessing.py
+++ MLHW3_Naive_Bayes_Classifier/forest_fire_prediction/preprocessing.py
@@ -36,7 +36,10 @@ def preprocesing():
             training_set[i][j] = float(training_set[i][j])
 
         if training_set[i][len(training_set[i])-1]: #prevent log(0) math domain exception
-            training_set[i][len(training_set[i])-1] = math.log10(training_set[i][len(training_set[i])-1]) #logarithmic transformation of the last data, since it is quite skew
+            training_set[i][len(training_set[i])-1] = int(math.log10(training_set[i][len(training_set[i])-1]))
+        else:
+            training_set[i][len(training_set[i])-1] = int(training_set[i][len(training_set[i])-1])
+            #logarithmic transformation of the last data, since it is quite skew
 
         training_set_predicted.append(training_set[i][len(training_set[i])-1])
         training_set[i] = training_set[i][4:12] #move the class away [4,12)
@@ -46,7 +49,10 @@ def preprocesing():
             testing_set[i][j] = float(testing_set[i][j])
 
         if testing_set[i][len(testing_set[i])-1]: #prevent log(0) math domain exception
-            testing_set[i][len(testing_set[i])-1] = math.log10(testing_set[i][len(testing_set[i])-1])
+            testing_set[i][len(testing_set[i])-1] = int(math.log10(testing_set[i][len(testing_set[i])-1]))
+        else:
+            training_set[i][len(training_set[i])-1] = int(training_set[i][len(training_set[i])-1])
+            #logarithmic transformation of the last data, since it is quite skew
 
         testing_set_predicted.append(testing_set[i][len(training_set[i])-1])
         testing_set[i] = testing_set[i][4:12] #move the class away [4,12)
@@ -61,80 +67,22 @@ def draw_PDF():
     training_set = np.array(training_set)#temporary convert to numpy array for plotting the pdf
 
     plt.figure() #make an empty canvas
+    print(training_set_predicted)
+    for i in range(0,8):
+        tf = training_set[:,i]
+        tf.sort()
+        plt.title(f' feature {i+1}: PDF')
+        mean = np.mean(tf)
+        std = np.std(tf)
+        pdf = stats.norm.pdf(tf,mean,std)
+        plt.subplot(2,4,i+1)
+        plt.plot(tf,pdf)
 
-    train_feature_1 = training_set[:,0]
-    train_feature_1.sort()
-    plt.title(" feature 1: FFMC PDF")
-    train_mean = np.mean(train_feature_1)
-    train_std = np.std(train_feature_1)
-    pdf = stats.norm.pdf(train_feature_1,train_mean,train_std) #use the scipy pdf function to show it
-    plt.subplot(2,4,1)
-    plt.plot(train_feature_1,pdf) #x axis `y axis
-
-    train_feature_2 = training_set[:,1]
-    train_feature_2.sort()
-    plt.title(" feature 2: DMC PDF")
-    train_mean = np.mean(train_feature_2)
-    train_std = np.std(train_feature_2)
-    pdf = stats.norm.pdf(train_feature_2,train_mean,train_std) #use the scipy pdf function to show it
-    plt.subplot(2,4,2)
-    plt.plot(train_feature_2,pdf) #x axis y axis
-
-    train_feature_3 = training_set[:,2]
-    train_feature_3.sort()
-    plt.title(" feature 3: DC PDF")
-    train_mean = np.mean(train_feature_3)
-    train_std = np.std(train_feature_3)
-    pdf = stats.norm.pdf(train_feature_3,train_mean,train_std) #use the scipy pdf function to show it
-    plt.subplot(2,4,3)
-    plt.plot(train_feature_3,pdf) #x axis y axis
-
-    train_feature_4 = training_set[:,3]
-    train_feature_4.sort()
-    plt.title(" feature 4: ISI PDF")
-    train_mean = np.mean(train_feature_4)
-    train_std = np.std(train_feature_4)
-    pdf = stats.norm.pdf(train_feature_4,train_mean,train_std) #use the scipy pdf function to show it
-    plt.subplot(2,4,4)
-    plt.plot(train_feature_4,pdf) #x axis y axis
-
-    train_feature_5 = training_set[:,4]
-    train_feature_5.sort()
-    plt.title(" feature 5: temp PDF")
-    train_mean = np.mean(train_feature_5)
-    train_std = np.std(train_feature_5)
-    pdf = stats.norm.pdf(train_feature_5,train_mean,train_std) #use the scipy pdf function to show it
-    plt.subplot(2,4,5)
-    plt.plot(train_feature_5,pdf) #x axis y axis
-
-    train_feature_6 = training_set[:,5]
-    train_feature_6.sort()
-    plt.title(" feature6: RH PDF")
-    train_mean = np.mean(train_feature_6)
-    train_std = np.std(train_feature_6)
-    pdf = stats.norm.pdf(train_feature_6,train_mean,train_std) #use the scipy pdf function to show it
-    plt.subplot(2,4,6)
-    plt.plot(train_feature_6,pdf) #x axis y axis
-
-    train_feature_7 = training_set[:,6]
-    train_feature_7.sort()
-    plt.title(" feature 7: wind PDF")
-    train_mean = np.mean(train_feature_7)
-    train_std = np.std(train_feature_7)
-    pdf = stats.norm.pdf(train_feature_7,train_mean,train_std) #use the scipy pdf function to show it
-    plt.subplot(2,4,7)
-    plt.plot(train_feature_7,pdf) #x axis y axis
-
-    train_feature_8 = training_set[:,7]
-    train_feature_8.sort()
-    plt.title(" feature 8: rain PDF")
-    train_mean = np.mean(train_feature_8)
-    train_std = np.std(train_feature_8)
-    pdf = stats.norm.pdf(train_feature_8,train_mean,train_std) #use the scipy pdf function to show it
-    plt.subplot(2,4,8)
-    plt.plot(train_feature_8,pdf) #x axis y axis
-
+    plt.tight_layout()
+    plt.savefig("PDF.png",dpi=600)
     plt.show()
+
+
 
 if __name__ == '__main__':
     preprocesing()
