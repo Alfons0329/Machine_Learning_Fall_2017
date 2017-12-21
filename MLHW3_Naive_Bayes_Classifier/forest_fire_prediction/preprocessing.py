1db916291fa897524599e30cff9afc2ba8122025
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-12-21 19:01:57 +0800

    Ready for the PDF of forest fire

--- MLHW3_Naive_Bayes_Classifier/forest_fire_prediction/preprocessing.py
+++ MLHW3_Naive_Bayes_Classifier/forest_fire_prediction/preprocessing.py
@@ -1,5 +1,7 @@
 """
 How to draw PDF?https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.histogram.html
+Subplot reference to http://noahsnail.com/2017/05/03/2017-5-3-matplotlib%E7%9A%84%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95(%E5%8D%81%E4%BA%8C)%E2%80%94%E2%80%94subplot%E7%BB%98%E5%88%B6%E5%A4%9A%E5%9B%BE/
+
 """
 import numpy as np
 import scipy as sp
@@ -58,67 +60,81 @@ def draw_PDF():
     global testing_set_predicted
     training_set = np.array(training_set)#temporary convert to numpy array for plotting the pdf
 
-    fid = plt.figure()
+    plt.figure() #make an empty canvas
+
     train_feature_1 = training_set[:,0]
     train_feature_1.sort()
-    plt.title("Forestfire feature 1: FFMC Probability Distribution Function")
+    plt.title(" feature 1: FFMC PDF")
     train_mean = np.mean(train_feature_1)
     train_std = np.std(train_feature_1)
     pdf = stats.norm.pdf(train_feature_1,train_mean,train_std) #use the scipy pdf function to show it
+    plt.subplot(2,4,1)
     plt.plot(train_feature_1,pdf) #x axis `y axis
-    plt.show()
-    input()
+
     train_feature_2 = training_set[:,1]
-    plt.title("Forestfire feature 2: DMC Probability Distribution Function")
+    train_feature_2.sort()
+    plt.title(" feature 2: DMC PDF")
     train_mean = np.mean(train_feature_2)
     train_std = np.std(train_feature_2)
     pdf = stats.norm.pdf(train_feature_2,train_mean,train_std) #use the scipy pdf function to show it
+    plt.subplot(2,4,2)
     plt.plot(train_feature_2,pdf) #x axis y axis
 
     train_feature_3 = training_set[:,2]
-    plt.title("Forestfire feature 2: DC Probability Distribution Function")
+    train_feature_3.sort()
+    plt.title(" feature 3: DC PDF")
     train_mean = np.mean(train_feature_3)
     train_std = np.std(train_feature_3)
     pdf = stats.norm.pdf(train_feature_3,train_mean,train_std) #use the scipy pdf function to show it
+    plt.subplot(2,4,3)
     plt.plot(train_feature_3,pdf) #x axis y axis
 
     train_feature_4 = training_set[:,3]
-    plt.title("Forestfire feature 3: ISI Probability Distribution Function")
+    train_feature_4.sort()
+    plt.title(" feature 4: ISI PDF")
     train_mean = np.mean(train_feature_4)
     train_std = np.std(train_feature_4)
     pdf = stats.norm.pdf(train_feature_4,train_mean,train_std) #use the scipy pdf function to show it
+    plt.subplot(2,4,4)
     plt.plot(train_feature_4,pdf) #x axis y axis
 
     train_feature_5 = training_set[:,4]
-    plt.title("Forestfire feature 4: temp Probability Distribution Function")
+    train_feature_5.sort()
+    plt.title(" feature 5: temp PDF")
     train_mean = np.mean(train_feature_5)
     train_std = np.std(train_feature_5)
     pdf = stats.norm.pdf(train_feature_5,train_mean,train_std) #use the scipy pdf function to show it
+    plt.subplot(2,4,5)
     plt.plot(train_feature_5,pdf) #x axis y axis
 
     train_feature_6 = training_set[:,5]
-    plt.title("Forestfire feature 5: RH Probability Distribution Function")
+    train_feature_6.sort()
+    plt.title(" feature6: RH PDF")
     train_mean = np.mean(train_feature_6)
     train_std = np.std(train_feature_6)
     pdf = stats.norm.pdf(train_feature_6,train_mean,train_std) #use the scipy pdf function to show it
+    plt.subplot(2,4,6)
     plt.plot(train_feature_6,pdf) #x axis y axis
 
     train_feature_7 = training_set[:,6]
-    plt.title("Forestfire feature 6: wind Probability Distribution Function")
+    train_feature_7.sort()
+    plt.title(" feature 7: wind PDF")
     train_mean = np.mean(train_feature_7)
     train_std = np.std(train_feature_7)
     pdf = stats.norm.pdf(train_feature_7,train_mean,train_std) #use the scipy pdf function to show it
+    plt.subplot(2,4,7)
     plt.plot(train_feature_7,pdf) #x axis y axis
 
     train_feature_8 = training_set[:,7]
-    plt.title("Forestfire feature 8: rain Probability Distribution Function")
+    train_feature_8.sort()
+    plt.title(" feature 8: rain PDF")
     train_mean = np.mean(train_feature_8)
     train_std = np.std(train_feature_8)
     pdf = stats.norm.pdf(train_feature_8,train_mean,train_std) #use the scipy pdf function to show it
+    plt.subplot(2,4,8)
     plt.plot(train_feature_8,pdf) #x axis y axis
 
     plt.show()
-    input()
 
 if __name__ == '__main__':
     preprocesing()
