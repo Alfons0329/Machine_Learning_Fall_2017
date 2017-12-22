3c259c7a05b9749113e85dabadeb2b7bfba36a5c
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-12-22 11:37:37 +0800

    Now writing the report

--- MLHW3_Naive_Bayes_Classifier/iris_dataset/naive_bayes_classifier.py
+++ MLHW3_Naive_Bayes_Classifier/iris_dataset/naive_bayes_classifier.py
@@ -1,5 +1,4 @@
-from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import random as rn
@@ -93,7 +92,7 @@ def draw_PDF():
 
     plt.tight_layout()
     plt.savefig("PDF.png",dpi=600)
-    plt.show()
+    #plt.show()
 
 def validate():
     #traing the model using GaussianNB
@@ -114,10 +113,10 @@ def validate():
         if(predicted_class == original_class):
             correct_prediction += 1
     print("Gaussian Naive Bayes Classifier Accuracy:",float(correct_prediction)/float(len(testing_set)))
-
+#########################################################################################################################################
     predicted_class_set = []
 
-    gnb = MultinomialNB(alpha=1) #using the "alpha = 1" param setting to set the laplacian smoothing
+    gnb = MultinomialNB(alpha = 1) #using the "alpha = 1" param setting to set the laplacian smoothing
     my_naive_bayes_classifier = gnb.fit(training_set, training_set_predicted)
     correct_prediction = 0
     predicted_class_set = gnb.predict(testing_set)
@@ -129,6 +128,22 @@ def validate():
         if(predicted_class == original_class):
             correct_prediction += 1
     print("Multinomial Naive Bayes Classifier Accuracy:",float(correct_prediction)/float(len(testing_set)))
+####################    #####################################################################################################################
+    predicted_class_set = []
+
+    gnb = BernoulliNB(alpha = 1) #using the "alpha = 1" param setting to set the laplacian smoothing
+    my_naive_bayes_classifier = gnb.fit(training_set, training_set_predicted)
+    correct_prediction = 0
+    predicted_class_set = gnb.predict(testing_set)
+
+    for i in range(len(testing_set)):
+        original_class = testing_set_predicted[i]
+        predicted_class = predicted_class_set[i]
+        #print("testing set",i," ",testing_set[i],"original_class ",original_class,"predicted_class",predicted_class)
+        if(predicted_class == original_class):
+            correct_prediction += 1
+    print("Bernoulli Naive Bayes Classifier Accuracy:",float(correct_prediction)/float(len(testing_set)))
+
 
 if __name__ == '__main__':
     preprocessing()
